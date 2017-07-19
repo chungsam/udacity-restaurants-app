9895161b8bc30531c19e3c9a6a2d8523ceedd775
Sam Chung <chungsam@outlook.com>
2017-07-19 11:17:24 -0700

    Fetch current location on initialization

--- js/app.js
+++ js/app.js
@@ -1,22 +1,77 @@
-/* Callback function for fetching Google Maps Javascript API */
+var map, infoWindow;
+var defaultCoords = {
+    lat: 50.891853,
+    lng: -130.0975733
+};
+
+var defaultZoomLevel = 15;
+
+/** App entry point is the callback for fetching Google Maps Javascript API **/
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: {
-            lat: -25.363,
-            lng: 131.044
-        }
+    var currentLocation = getCurrentLocation();
+
+    /* Initial rendering of Google Map */
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 8,
+        center: defaultCoords
+    });
+
+    /* Google Places API Service */
+    var placesService = new google.maps.places.PlacesService(map);
+
+    placesService.nearbySearch({
+        location: defaultCoords,
+        radius: 5000,
+        type: ['food']
+    }, function (data) {
+        console.log(data);
     });
 }
 
-/* Main application */
-$(function () {
-    var ViewModel = function () {
-        this.searchAddress = ko.observable("");
-        this.searchRestaurant = ko.observable("");
+function getCurrentLocation() {
+    var location = {};
 
-        
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (pos) {
+            location = pos;
+
+            map = new google.maps.Map(document.getElementById('map'), {
+                zoom: defaultZoomLevel,
+                center: {
+                    lat: pos.coords.latitude,
+                    lng: pos.coords.longitude
+                }
+            });
+        });
+    } else {
+        map = new google.maps.Map(document.getElementById('map'), {
+            zoom: defaultZoomLevel,
+            center: defaultCoords
+        });
     };
 
-    ko.applyBindings(new ViewModel());
-})
+    return location;
+}
+
+function createMarker() {
+
+}
+
+/* Define Knockout ViewModel */
+var ViewModel = function () {
+    // this.restaurants = ko.observableArray(restaurants);
+    this.currentLocation = ko.observable(getCurrentLocation());
+    this.addressInput = ko.observable("test");
+    this.restaurantNameInput = ko.observable("");
+
+    this.searchByAddress = function () {
+        searchByAddress(this.addressInput);
+    };
+
+    this.searchByRestaurantName = function () {
+
+    };
+
+};
+
+ko.applyBindings(new ViewModel());
